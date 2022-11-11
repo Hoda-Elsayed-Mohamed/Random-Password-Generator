bfb2738103817b0f5da02edba04a55f1ffede345
Hoda-Elsayed-Mohamed <96476988+Hoda-Elsayed-Mohamed@users.noreply.github.com>
2022-11-11 12:30:12 +0200

    upLoad

--- index.js
+++ index.js
@@ -1,34 +1,91 @@
-let inputEl =document.getElementById('password')
-let btnEl =document.querySelector('.generate')
-let spanEl =document.querySelector('span')
-let popup =document.querySelector('.popup')
+let wrapper = document.querySelector('.container');
+let InputEl = document.querySelector('.enter-task');
+let containerEl = document.querySelector('.reverse');
+let checkedInput = document.getElementById('checked');
+let list = JSON.parse(localStorage.getItem('list'))
 
-btnEl.addEventListener('click', createRandom)
-
-function createRandom(){
-const randomLetters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let length = 14;
-    var generatedPassword='';
-
-    for(let i=0; i<length; i++){
-  let b = Math.floor(Math.random()*randomLetters.length);
-  generatedPassword += randomLetters.substring(b, b-1);
-    }
-inputEl.value = generatedPassword
-
+if(list){
+  list.forEach(task => {
+    createTask(task)
+  });
 }
+// localStorage.setItem('createTask', 'createTask()')
+function createTask(task){
+  let insertedText = InputEl.value;
 
-spanEl.addEventListener('click', ()=>{
-  inputEl.select();
-  inputEl.setSelectionRange(0, 9999)
-  navigator.clipboard.writeText(inputEl.value)
-  if(inputEl.select){
-    popup.classList.add('active')
-  popup.textContent= inputEl.value + ' Copied!'
-  setTimeout(() => {
-    popup.classList.remove('active')
-    popup.style.transition = 'all 0.7s'
-  }, 3000);
+  if(task){
+    insertedText = task.name
   }
   
+  
+  if(insertedText!== ''){
+    
+    var newTask = document.createElement('div')
+
+    if(task && task.checked){
+     newTask.classList.add('active')
+   }
+     containerEl.appendChild(newTask);
+     newTask.classList.add('new-task')
+     newTask.innerHTML = `<span>${insertedText}</span>  <div class="control">
+     <input type="checkbox" name="" id="checked" checked>
+     <i class="fa-solid fa-trash"></i>
+     </div> `
+     
+     InputEl.value = ''
+    
+    // adding eventlistener to the trash icon
+    if(containerEl.contains(newTask)){
+      let iconEl =document.querySelectorAll('.new-task i')
+      iconEl.forEach(element => {
+        element.addEventListener('click',(e)=>{
+          if(newTask.contains(e.target)){
+            newTask.remove()
+            upDateLocalStorage() 
+          }
+        })
+      });
+    }
+    
+    // adding eventlistener to the checkbox
+    if(containerEl.contains(newTask)){
+      let checkedInputs =document.querySelectorAll('.new-task input')
+      checkedInputs.forEach(input => {
+        input.addEventListener('click',(e)=>{
+          e.preventDefault()
+          if(newTask.classList.contains('active')){
+          
+            newTask.classList.remove('active')
+            upDateLocalStorage() 
+          }else{
+            
+            newTask.classList.add('active')
+            upDateLocalStorage() 
+          }
+        })
+      });
+    }
+  }
+  upDateLocalStorage() 
+}
+
+
+function upDateLocalStorage(){
+  const divs = document.querySelectorAll('.new-task')
+    list = []
+  divs.forEach(divEl => {
+    list.push({
+      name: divEl.innerText,
+      checked: divEl.classList.contains('active')
+    })
+  });
+  localStorage.setItem('list', JSON.stringify(list))
+}
+
+wrapper.addEventListener('submit', (event)=>{
+  event.preventDefault()
+
+  createTask()
 })
+
+// console.log(localStorage.getItem('create'));
